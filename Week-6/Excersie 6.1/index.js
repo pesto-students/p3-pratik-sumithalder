c50475e013eec9879fca13f235432691601fd5bc
Sumit Halder <sumithalder100@gmail.com>
2022-08-05 19:54:50 +0530

    Add leetcode link

--- Week-6/Excersie 6.1/index.js
+++ Week-6/Excersie 6.1/index.js
@@ -8,3 +8,6 @@ var maxSubArray = function(nums) {
 let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
 console.log("Max sub array is " + maxSubArray(nums)); // Max sub array is 6
+
+
+// https://leetcode.com/submissions/detail/760326289/
